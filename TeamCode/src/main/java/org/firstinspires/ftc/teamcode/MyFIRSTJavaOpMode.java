d5d190769945eef5dace3393fdb69431a5375c9a
Sushant Pisupati <26spisupati@stratfordschools.net>
2022-10-22 15:07:46 -0700

    sushant makes another encode. ticks, set opwer functions, and initialized all four motora and set target. run to position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -1,22 +1,23 @@
+//autonomous mode, not op mode
+//
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name = "EncoderTest2", group = "Auton")
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     RobotHardware robot = new RobotHardware();
     DcMotor leftFront;
     DcMotor rightFront;
     DcMotor leftBack;
     DcMotor rightBack;
-    DcMotor crane;
-    Servo claw;
-    BNO055IMU imu;
-    ElapsedTime runtime;
+    //DcMotor crane;
+    //Servo claw;
+    //BNO055IMU imu;
+    //ElapsedTime runtime;
     static final double COUNTS_PER_MOTOR_REV  = 28.0;
     static final double DRIVE_GEAR_REDUCTION = 30.24;
     static final double WHEEL_CIRCUMFERENCE_MM = 3.78 * 3.14;
@@ -30,15 +31,15 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         robot.rightFront = hardwareMap.get(DcMotor.class, "robot.rightFront");
         robot.leftBack = hardwareMap.get(DcMotor.class, "robot.leftBack");
         robot.rightBack = hardwareMap.get(DcMotor.class, "robot.rightBack");
-        crane = hardwareMap.get(DcMotor.class, "crane");
-        claw = hardwareMap.get(Servo.class, "claw");
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        //crane = hardwareMap.get(DcMotor.class, "crane");
+        //claw = hardwareMap.get(Servo.class, "claw");
+        //imu = hardwareMap.get(BNO055IMU.class, "imu");
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection((DcMotorSimple.Direction.REVERSE));
         int lefttarget = (int)(610 * COUNTS_PER_MM);
         int righttarget = (int)(610 * COUNTS_PER_MM);
-        double ltps = (175/60) * COUNTS_PER_WHEEL_REV;
-        double rtps = (175/60) * COUNTS_PER_WHEEL_REV;
+        double ltps = (175.0/60) * COUNTS_PER_WHEEL_REV;
+        double rtps = (175.0/60) * COUNTS_PER_WHEEL_REV;
         // Put initialization blocks here
         waitForStart();
         // Put run blocks here
@@ -71,8 +72,8 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
 
         int leftTarget = (int)(300 * COUNTS_PER_MM);
         int rightTarget = (int)( -300 * COUNTS_PER_MM);
-        double LTPS = (100/ 60) * COUNTS_PER_WHEEL_REV;
-        double RTPS = (70/ 60) * COUNTS_PER_WHEEL_REV;
+        double LTPS = (100.0/ 60) * COUNTS_PER_WHEEL_REV;
+        double RTPS = (70.0/ 60) * COUNTS_PER_WHEEL_REV;
 
         leftFront.setTargetPosition(leftTarget);
         leftBack.setTargetPosition(leftTarget);
