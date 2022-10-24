4783bc85a1c1becc5b3f97a51fe0b33c50b8b9eb
Rian Butala <rianbutala@gmail.com>
2022-10-24 08:36:09 -0700

    quick commit - my dad and I found a flaw in our encoder movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -11,21 +11,26 @@ public class EncoderTest extends OpMode {
 
     //@Override whenever you create a method
     @Override
-    //What happens when you initalize program
+    //What happens when you initialize program
     public void init(){
         //Initialize hardwareMap
         robot.init(hardwareMap);
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+        robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.ViperSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        telemetry.addData("Status", "Running using Encoder");
+        telemetry.update();
         robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.ViperSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.ViperSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
@@ -35,12 +40,14 @@ public class EncoderTest extends OpMode {
 
     @Override
     public void start(){
-
+        telemetry.addData("Status" , "Attempting one rotation of spin");
+        robot.encoderMovements(11.8692, 0.5);
+        //this.sleep(5000);
     }
 
     @Override
     public void loop() {
-        robot.encoderMovements(11.8692, 0.5);
+
     }
 
     @Override
