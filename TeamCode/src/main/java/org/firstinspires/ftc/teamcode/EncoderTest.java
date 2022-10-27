7a3193d5b9c54dfc35e0828213cafdc88519fdc6
Rian Butala <rianbutala@gmail.com>
2022-10-27 15:56:37 -0700

    before zoom call 10/27/22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+ 
 @Autonomous(name = "EncoderTest", group = "Auton")
 
 public class EncoderTest extends OpMode {
