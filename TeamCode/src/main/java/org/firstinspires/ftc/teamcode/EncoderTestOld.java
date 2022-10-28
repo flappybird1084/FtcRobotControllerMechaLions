556c2d706255121e7ffc7d375ea50707ddb6f2cd
Rian Butala <rianbutala@gmail.com>
2022-10-27 17:09:02 -0700

    end of day - oct 27 22. tried fixing the encoders for the millionth time but nothing seems to be working. will try again on saturday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTestOld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTestOld.java
@@ -1,19 +1,21 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
     @Autonomous(name = "EncoderTestOld", group = "Auton")
 
-    public class EncoderTestOld extends OpMode {
+    public class EncoderTestOld extends LinearOpMode {
         RobotHardware robot = new RobotHardware();
 
         //@Override whenever you create a method
-        @Override
         //What happens when you initalize program
-        public void init(){
+        @Override
+        public void runOpMode(){
             //Initialize hardwareMap
             robot.init(hardwareMap);
+            waitForStart();
             robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -26,27 +28,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
             robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             robot.ViperSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+            robot.encoderMovements(70, 1);
         }
 
-        @Override
-        public void init_loop(){
-
-        }
-
-        @Override
-        public void start(){
-            robot.all_full_power();
-        }
-
-        @Override
-        public void loop() {
-
-            //robot.encoderMovements(70, 1);
-        }
-
-        @Override
-        public void stop() {
-
-        }
     }
 
