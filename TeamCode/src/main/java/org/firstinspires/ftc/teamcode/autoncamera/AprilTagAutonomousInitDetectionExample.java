b61096e5fc6bea22771b839a221f2771b75575e3
rianbutala <rianbutala@gmail.com>
2022-11-12 13:34:19 -0800

    woo we made it look at apriltags and move accordingly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoncamera/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoncamera/AprilTagAutonomousInitDetectionExample.java
@@ -21,10 +21,13 @@
 
 package org.firstinspires.ftc.teamcode.autoncamera;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -33,11 +36,14 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
-@TeleOp
-public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
-{
+@Autonomous(name = "AprilTagAutonomousInitDetectionExample", group = "Auton")
+public class AprilTagAutonomousInitDetectionExample extends LinearOpMode {
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    RobotHardware robot = new RobotHardware();
+    private ElapsedTime runtime = new ElapsedTime();
+    static private int timeoutS = 10000; // APB: not optimized
+    private int encoderDist = 70;
 
     static final double FEET_PER_METER = 3.28084;
 
@@ -60,25 +66,57 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
 
     AprilTagDetection tagOfInterest = null;
 
+    void tagToTelemetry(AprilTagDetection detection) {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z * FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+    public void waitForEncoderComplete() {
+        //sleep(10000);
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+        while (opModeIsActive() && (runtime.seconds() < timeoutS) && robot.isAnyBusy()) {
+            // Display it for the driver.
+            //telemetry.addData("Path1",  "Running to %7d :%7d", encoderDist,  encoderDist);
+            //telemetry.addData("Path2",  "Running at %7d :%7d",
+            //        robot.leftFront.getCurrentPosition(),
+            //        robot.rightFront.getCurrentPosition());
+            //telemetry.update();
+        }
+        telemetry.addData("Step: ", "Done");
+        telemetry.update();
+        sleep(500);
+        //robot.zero();
+    }
+
+
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
+        int position = 0;
+        robot.init(hardwareMap);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
         camera.setPipeline(aprilTagDetectionPipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened() {
+                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode)
-            {
+            public void onError(int errorCode) {
 
             }
         });
@@ -89,55 +127,40 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
          * The INIT-loop:
          * This REPLACES waitForStart!
          */
-        while (!isStarted() && !isStopRequested())
-        {
+        while (!isStarted() && !isStopRequested()) {
             ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
 
-            if(currentDetections.size() != 0)
-            {
+            if (currentDetections.size() != 0) {
                 boolean tagFound = false;
 
-                for(AprilTagDetection tag : currentDetections)
-                {
-                    if(tag.id == LEFT || tag.id == RIGHT || tag.id == MIDDLE)
-                        {
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == LEFT || tag.id == RIGHT || tag.id == MIDDLE) {
                         tagOfInterest = tag;
                         tagFound = true;
                         break;
                     }
                 }
 
-                if(tagFound)
-                {
+                if (tagFound) {
                     telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
                     tagToTelemetry(tagOfInterest);
-                }
-                else
-                {
+                } else {
                     telemetry.addLine("Don't see tag of interest :(");
 
-                    if(tagOfInterest == null)
-                    {
+                    if (tagOfInterest == null) {
                         telemetry.addLine("(The tag has never been seen)");
-                    }
-                    else
-                    {
+                    } else {
                         telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                         tagToTelemetry(tagOfInterest);
                     }
                 }
 
-            }
-            else
-            {
+            } else {
                 telemetry.addLine("Don't see tag of interest :(");
 
-                if(tagOfInterest == null)
-                {
+                if (tagOfInterest == null) {
                     telemetry.addLine("(The tag has never been seen)");
-                }
-                else
-                {
+                } else {
                     telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                     tagToTelemetry(tagOfInterest);
                 }
@@ -154,46 +177,76 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
          */
 
         /* Update the telemetry */
-        if(tagOfInterest != null)
-        {
+        if (tagOfInterest != null) {
             telemetry.addLine("Tag snapshot:\n");
             tagToTelemetry(tagOfInterest);
             telemetry.update();
-        }
-        else
-        {
+        } else {
             telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
             telemetry.update();
         }
 
         /* Actually do something useful */
 
-        if(tagOfInterest == null || tagOfInterest.id == LEFT) {
+        if (tagOfInterest == null || tagOfInterest.id == LEFT) {
             //left trajectory
-        }
-
-        else if (tagOfInterest.id == RIGHT) {
+            position = 1;
+        } else if (tagOfInterest.id == RIGHT) {
             //right trajectory
-        }
-
-        else if (tagOfInterest.id == MIDDLE) {
+            position = 3;
+        } else if (tagOfInterest.id == MIDDLE) {
             // middle trajectory
+            position = 2;
         }
 
-        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending
         while (opModeIsActive()) {
             sleep(20);
-        }
-    }
+        } */
+        waitForStart();
 
-    void tagToTelemetry(AprilTagDetection detection)
-    {
-        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
-        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
-        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
-        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
-        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
-        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
-        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+        //robot.encoderMovements(encoderDist, 1);
+        if (position == 1) {
+            robot.moveDirectionBlocks(telemetry, 1, "left");
+            telemetry.addData("Step: ", "Step #1");
+            telemetry.update();
+            waitForEncoderComplete();
+            robot.moveDirectionBlocks(telemetry, 1, "forward");
+            telemetry.addData("Step: ", "Step #2");
+            telemetry.update();
+            waitForEncoderComplete();
+            telemetry.addData("Step: ", "Finished");
+            telemetry.update();
+        } else if (position == 2) {
+            robot.moveDirectionBlocks(telemetry, 1, "right");
+            telemetry.addData("Step: ", "Step #1");
+            telemetry.update();
+            waitForEncoderComplete();
+            robot.moveDirectionBlocks(telemetry, 1, "forward");
+            telemetry.addData("Step: ", "Step #2");
+            telemetry.update();
+            robot.moveDirectionBlocks(telemetry, 1, "forward");
+            telemetry.addData("Step: ", "Step #3");
+            telemetry.update();
+            robot.moveDirectionBlocks(telemetry, 1, "left");
+            telemetry.addData("Step: ", "Step #4");
+            telemetry.update();
+            waitForEncoderComplete();
+            telemetry.addData("Step: ", "Finished");
+            telemetry.update();
+        } else if (position == 3) {
+            robot.moveDirectionBlocks(telemetry, 1, "right");
+            telemetry.addData("Step: ", "Step #1");
+            telemetry.update();
+            waitForEncoderComplete();
+            robot.moveDirectionBlocks(telemetry, 1, "forward");
+            telemetry.addData("Step: ", "Step #2");
+            telemetry.update();
+            waitForEncoderComplete();
+            telemetry.addData("Step: ", "Finished");
+            telemetry.update();
+        }
+
+
     }
 }
