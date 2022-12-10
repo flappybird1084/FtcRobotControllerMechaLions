0b9c25b24bf13965c3d7315d0eec4418ebefd364
rianbutala <rianbutala@gmail.com>
2022-12-10 13:30:51 -0800

    made speedscaling work with strafing, in theory. now handing off to ant's computer so that we can code in the comp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -55,6 +55,10 @@ public class TeleOpTest  extends OpMode {
         double rightStick = -gamepad1.right_stick_y;
         double leftStick = -gamepad1.left_stick_y;
 
+        robot.ViperSlide.setPower(-gamepad2.left_stick_y);
+        robot.ViperSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        speedScaling = (Math.abs(gamepad2.right_stick_y)*3/5) + 0.4;
+
         if(gamepad1.right_bumper) {
             // Move Right
             robot.leftFront.setPower(speedScaling);
@@ -94,9 +98,7 @@ public class TeleOpTest  extends OpMode {
             robot.ViperSlide.setPower(0); // not in use anymore, rebind if you want
         }
 
-        robot.ViperSlide.setPower(-gamepad2.left_stick_y);
-        robot.ViperSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        speedScaling = (Math.abs(gamepad2.right_stick_y)*3/5) + 0.4;
+
 
 
 
